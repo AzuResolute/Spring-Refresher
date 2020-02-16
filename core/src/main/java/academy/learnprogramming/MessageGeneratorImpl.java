05faad5e16c438bb424703a87df91e039a1c75e3
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2020-02-16 16:04:47 -0500

    Added logic

--- core/src/main/java/academy/learnprogramming/MessageGeneratorImpl.java
+++ core/src/main/java/academy/learnprogramming/MessageGeneratorImpl.java
@@ -21,11 +21,21 @@ public class MessageGeneratorImpl implements MessageGenerator {
 
     @Override
     public String getMainMessage() {
-        return "Main Message Received";
+        return "Number is between " +
+                game.getSmallest() +
+                " and " +
+                game.getLargest() +
+                ". Can you guess?";
     }
 
     @Override
     public String getResultMessage() {
-        return "Result Message Received";
+        return game.isGameWon() ? "You guess it! The number was" + game.getNumber() :
+            game.isGameLost() ? "You lost. The game was " + game.getNumber() :
+                !game.isValidNumberRange() ? "Invalid number range!" :
+                    game.getRemainingGuesses() == guessCount ? "What is your first guess?" :
+                        game.getGuess() < game.getNumber() ?
+                            "Higher! You have " + game.getRemainingGuesses() + " guesses left" :
+                            "Lower! You have " + game.getRemainingGuesses() + " guesses left";
     }
 }
