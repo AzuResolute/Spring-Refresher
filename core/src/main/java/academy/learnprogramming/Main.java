e7ab3f6dd035113565ab2293b7c3da2b1522e021
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2020-02-15 21:10:53 -0500

    First run of Spring Review - Guess the Number with XML Bean

--- core/src/main/java/academy/learnprogramming/Main.java
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -2,11 +2,25 @@ package academy.learnprogramming;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
+    private static final String CONFIG_LOCATION = "beans.xml";
 
     public static void main(String[] args) {
         log.info("Guess the number game, activate!");
+
+        ConfigurableApplicationContext context =
+                new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+
+        NumberGenerator numGen = context.getBean("numberGenerator", NumberGenerator.class);
+
+        int number = numGen.next();
+
+        log.info("randomly generated number = {}", number);
+
+        context.close();
     }
 }
